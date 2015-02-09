ec1187fa4e36cc278b48529ccdcfa4f4255f7dc8
psnideriii <payton.snider@gmail.com>
2015-02-08 22:49:20 -0800

    created deploy file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -42,18 +42,18 @@ def _update_settings(source_folder, site_name):
 	
 def _update_virtualenv(source_folder):
 	virtualenv_folder = source_folder + '/../virtualenv'
-	if not exists(virtualenv_folder + '/bin/pip'):
+	if not exists(virtualenv_folder + '/bin/pip3'):
 		run('virtualenv --python=python3 %s' % (virtualenv_folder,))
 	run('%s/bin/pip install -r %s/requirements.txt' % (
 			virtualenv_folder, source_folder
 	))
 	
 def _update_static_files(source_folder):
-	run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
+	run('cd %s && ../virtualenv/bin/python manage.py collectstatic --noinput' % (
 		source_folder,
 	))
 	
 def _update_database(source_folder):
-	run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
+	run('cd %s && ../virtualenv/bin/python manage.py migrate --noinput' % (
 		source_folder,
 	))
